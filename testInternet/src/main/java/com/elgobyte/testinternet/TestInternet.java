9d2d443b299d26d15b1abe01f15c197e2d90e2f5
elgobyte <57178092+elgobyte@users.noreply.github.com>
2021-06-02 16:26:39 +0500

    Initial commit

---
+++ testInternet/src/main/java/com/elgobyte/testinternet/TestInternet.java
@@ -0,0 +1,12 @@
+package com.elgobyte.testinternet;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class TestInternet {
+
+
+    public TestInternet(Context context) {
+        Toast.makeText(context, "Hi, I am working", Toast.LENGTH_SHORT).show();
+    }
+}
